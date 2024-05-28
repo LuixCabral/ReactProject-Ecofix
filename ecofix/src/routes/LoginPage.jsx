d658f867d2d3c35b9a5158fde2f6ed91969bc837
Andre-PI <andrelucas_PI@proton.me>
2024-05-28 00:30:07 -0300

    iniciando a criação do cadastro

--- ecofix/src/routes/LoginPage.jsx
+++ ecofix/src/routes/LoginPage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import {supabase} from '../components/DatabaseConnection'
 
 export default function LoginPage(){
+    const[isLoginClicked, setLoginClicked] = useState(true);
     const [email, setEmail] = useState('');
     //const [password, setPassword] = useState('');
     const [profiles, setProfiles] = useState([]);
@@ -14,36 +15,56 @@ export default function LoginPage(){
     },[])
 
     async function getProfiles() {
-        const {data} = await supabase.from('profiles').select().eq('username', 'lucas');
+        //not working yet
+        const {data} = await supabase.from('profiles').select();
         setProfiles(data);
         console.log("this: " ,profiles)
     }
 
-    const handleEvent = (event) => {
+    let changeToSignUp = (event) =>{
+        event.preventDefault();
+        setLoginClicked(false);
+    }
+    const notReloadThePageEvent = (event) => {
         event.preventDefault();
     }
 
+    function changeForm(){
+        //incompleto
+        if(isLoginClicked == true){
+            <form onSubmit={notReloadThePageEvent}>
+                <input type="text" />
+                <input type="password" />
+                <input type="text" />
+            </form>
+        }
+        <form onSubmit={notReloadThePageEvent} >
+            <input type="text" id="login" className="fadeIn second" name="login" placeholder="Email" onChange={e => setEmail(e.target.value)}/>
+            <input type="password" id="password" className="fadeIn third" name="login" placeholder="Senha"/>
+            <input type="submit" className="fadeIn fourth" value="Log In"/>
+        </form> 
+    }
+    
     return(
         <body>
-        <section className="container">
-            <div className="login-container">
-                <div className="circle circle-one"></div>
-                <div className="form-container">
-                    <h1 className="opacity">Bem-vindo a ecofix</h1>
-                    <form onSubmit={handleEvent}>
-                        <input type="text" placeholder="USERNAME" value={email} onChange={e => setEmail(e.target.value)}/>
+            <div className="wrapper fadeInDown">
+            <div id="formContent">
 
-                        <input type="password" placeholder="PASSWORD"/>
-                        <button type='submit' className="opacity">Entrar</button>
-                    </form>
-                    <div className="register-forget opacity">
-                        <a href="">Cadastrar-se</a>
-                        <a href="">Esqueci minha senha</a>
-                    </div>
+                <h2 className="active"> Entrar </h2>
+                <h2 onClick={changeToSignUp} className="inactive underlineHover">Cadastrar </h2>
+
+
+                <div className="fadeIn first">
+                <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
                 </div>
+                    <changeForm/>
+                <div id="formFooter">
+                <a className="underlineHover" href="#">Esqueceu a senha?</a>
+                </div>
+
             </div>
-            <div className="theme-btn-container"></div>
-        </section>
+            </div>
         </body>
+        
     )
 }
