415e0614e180e6f664784c99eda42154c30bdc35
Luis Victor <luisvictor10@outlook.com>
2024-05-29 16:02:46 -0300

    a função agora realiza download de um aruivo selecionado

--- ecofix/src/components/BotaoDownload.js
+++ ecofix/src/components/BotaoDownload.js
@@ -1,10 +1,14 @@
 import axios from "axios"
 import fileDownload from 'js-file-download'
+import config from "./configs"
 
 
-function handleDownload(url,filename) {
-    axios.get(url)
+function handleDownload(configs) {
+    const {url, filename} = config
+    console.log(config.url)
+    axios.get(config.url)
     .then((res =>{fileDownload(res.data,filename)}))
+
 }
 
 
---
+++ ecofix/src/components/configs.js
@@ -0,0 +1,6 @@
+const config = {
+    url:"./testes/teste1.txt",
+    filename: 'teste1.txt'
+};
+
+export default config
