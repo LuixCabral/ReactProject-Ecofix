247ff53bcb2df2197d77c79207b09e116b58b118
LuixCabral <134115618+LuixCabral@users.noreply.github.com>
2024-06-06 17:02:49 -0300

    upload conectado com o firebase

--- ecofix/src/components/upload.js
+++ ecofix/src/components/upload.js
@@ -1,5 +1,17 @@
-function uploadArquivos(){
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+import app from "./DatabaseConnection";
+
+function uploadArquivos(file) {
+    const storage = getStorage(app);
+    const storageRef = ref(storage, 'testes/teste1.txt');
+    const myFile = new File(["conteúdo do arquivo"], "teste1.txt");
+
+    uploadBytes(storageRef, file).then((snapshot) => {
+        console.log('Upload realizado com sucesso!', snapshot);
+    }).catch((error) => {
+        console.error('Erro no upload:', error);
+    });
 
 }
 
-export default uploadArquivos
+export default uploadArquivos;
