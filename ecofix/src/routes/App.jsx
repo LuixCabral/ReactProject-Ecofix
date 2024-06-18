6fc900447451b2058d513fea5e715e11170b09d0
Andre-PI <andrelucas_PI@proton.me>
2024-06-18 12:17:34 -0300

    inutilçização do App.jsx(nao remover, provavel erro)

--- ecofix/src/routes/App.jsx
+++ ecofix/src/routes/App.jsx
@@ -1,20 +1,22 @@
-import '../styles/App.css';
 import { useEffect } from 'react';
+import '../styles/App.css';
+
 import { useNavigate } from 'react-router-dom';
 
 
 export default function App() {
   const navigate = useNavigate();
   function switchPage(){
-    navigate('entrar');
+    navigate('/entrar');
   }
+  useEffect(() => {
+    switchPage();
+  })
 
   return (
     <>
     <h1>Bem-vindo a ecofix</h1>
-    <button onClick={switchPage}>Login</button>
-    <button onClick={handleDownload}>Download</button>
-    
+    <button onClick={switchPage}>Login</button>   
     </>
   )
 }
